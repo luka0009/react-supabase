57d17eec5926bb0c219b94bf7793c4ed425976df
luka <lukabarbakadze18@gmail.com>
2023-06-23 21:51:18 +0400

    fetching and displaying data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
 import supabase from "../config/superbase";
+import { useEffect, useState } from "react";
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
   console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("table 1 ").select();
+
+      if (error) {
+        setFetchError(error);
+        console.log(fetchError);
+        setSmoothies(null);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+        console.log(smoothies);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
+  console.log("smoothies", smoothies);
 
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
