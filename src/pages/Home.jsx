73090d471011f0df42cf99588fa05a4cfe4233a9
luka <lukabarbakadze18@gmail.com>
2023-06-23 21:29:55 +0400

    connecting to superbase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import supabase from "../config/superbase";
+
 const Home = () => {
-    return (
-      <div className="page home">
-        <h2>Home</h2>
-      </div>
-    )
-  }
-  
-  export default Home
+  console.log(supabase);
+
+  return (
+    <div className="page home">
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    PROJECT_URL: process.env.VALUE,
+    API_KEY: process.env.VALUE,
+  },
   plugins: [react()],
-})
+});
