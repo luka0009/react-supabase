08e5f9e7d00fe7e1084de9d95b6253b1cb7a880b
luka <lukabarbakadze18@gmail.com>
2023-06-23 23:26:40 +0400

    deleting data

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,6 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import supabase from "../config/superbase";
 
-const SmoothieCard = ({ smoothie }) => {
+const SmoothieCard = ({ smoothie, onDelete }) => {
+  const habdleDelete = async () => {
+    const { data, error } = await supabase
+      .from("table 1")
+      .delete()
+      .eq("id", smoothie.id);
+
+    if (error) {
+      console.log(error);
+    }
+    // if (data) {
+    //   console.log(data);
+    //   onDelete(smoothie.id);
+    //   window.location.reload();
+    // }
+    onDelete(smoothie.id);
+  };
+
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
@@ -10,9 +28,12 @@ const SmoothieCard = ({ smoothie }) => {
         <Link to={"/" + smoothie.id}>
           <i className="material-icons">edit</i>
         </Link>
+        <i className="material-icons" onClick={habdleDelete}>
+          delete
+        </i>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SmoothieCard
+export default SmoothieCard;
